bda034a22f53b9652ee555b1d39f2f86a1f4e296
Cameron Ekblad <cekb635@aucklanduni.ac.nz>
2015-12-11 15:44:07 +1300

    Searching simple Rao table working.

--- hiC_query.py
+++ hiC_query.py
@@ -6,7 +6,7 @@ from sets import Set
 
 def build_snp_index():
 	print "Building SNP index..."
-	snp_index_db = sqlite3.connect("snpIndex.db")
+	snp_index_db = sqlite3.connect("snpIndex.test.db")
 	snp_index = snp_index_db.cursor()
 	snp_index.execute("CREATE TABLE snps (rsID text, chr text, locus integer)")
 	snp_index.execute("CREATE UNIQUE INDEX id ON snps (rsID)")
@@ -27,10 +27,10 @@ def process_inputs(inputs):
 	loci = []
 	for input in inputs:
 		if input.startswith("rs"):
-			if not os.path.isfile("snpIndex.db"): #Need to build SNP index
+			if not os.path.isfile("snpIndex.test.db"): #Need to build SNP index
 				build_snp_index()
 			print "Loading SNP index..."
-			snp_index_db = sqlite3.connect("snpIndex.db")
+			snp_index_db = sqlite3.connect("snpIndex.test.db")
 			snp_index_db.text_factory = str
 			snp_index = snp_index_db.cursor()
 			break
@@ -38,34 +38,43 @@ def process_inputs(inputs):
 		print "Processing " + input
 		if input.startswith("rs"):
 			snp_index.execute("SELECT chr, locus FROM snps WHERE rsID=?",(input,))
-			loci.append(snp_index.fetchone())
+			locus = snp_index.fetchone()
+			if locus == None:
+				print "Error: %s does not exist in database." % input
+			else:
+				loci.append(locus)
 		else:
 			loci.append((input[input.find("chr")+3:input.find(':')],int(input[input.find(':')+1:])))
 	return loci
 	
-def find_interactions(args):
-	chr = args.locus[args.locus.find("chr")+3:args.locus.find(':')]
-	locus = int(args.locus[args.locus.find(':')+1:])
-	rao_table = open(args.table_fp,'r')
-	interactions = []
-	for line in rao_table:
-		interaction = line.strip().split(' ')
-		chr1 = interaction[2]
-		chr2 = interaction[6]
-		locus1 = int(interaction[3])
-		locus2 = int(interaction[7])
-		if ((chr1 == chr and (locus1-args.distance < locus and locus1+args.distance > locus)) or (chr2 == chr and (locus2-args.distance < locus and locus2+args.distance > locus))) and (int(interaction[9]) > args.mapq_cutoff and int(interaction[10]) > args.mapq_cutoff):
-			if chr1 == chr and (locus1-args.distance < locus and locus1+args.distance > locus):
-				interactions.append(Interaction(chr2,locus2))
-			else:
-				interactions.append(Interaction(chr1,locus1))
-		return interactions
+def find_interactions(loci,table_fp,distance,mapq_cutoff):
+	interactions = {}
+	for locus in loci:
+		rao_table = open(table_fp,'r')
+		chr = locus[0]
+		coord = int(locus[1])
+		interactions[locus] = []
+		print "Processing " + chr + ' ' + str(coord)
+		for line in rao_table:
+			interaction = line.strip().split(' ')
+			chr1 = interaction[2]
+			chr2 = interaction[6]
+			coord1 = int(interaction[3])
+			coord2 = int(interaction[7])
+			print '\t' + chr1 + ' ' + str(coord1) + '\n\t' + chr2 + ' ' + str(coord2)
+			if ((chr1 == chr and (coord1-distance < coord and coord1+distance > coord)) or (chr2 == chr and (coord2-distance < coord and coord2+distance > coord))) and (int(interaction[9]) > mapq_cutoff and int(interaction[10]) > mapq_cutoff):
+				if chr1 == chr and (coord1-distance < coord and coord1+distance > coord):
+					interactions[locus].append((chr2,coord2))
+				else:
+					interactions[locus].append((chr1,coord1))
+		rao_table.close()
+	return interactions
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="")
 	parser.add_argument("-i","--inputs",nargs='+',help="The the dbSNP ID or loci of interest in the format \"chr<x>:<locus>\"")
-	parser.add_argument("-t","--table_fp",default="HIC059_mapq150_chr21.txt",help="TEMP: The Rao table from which to pull connections...")
+	parser.add_argument("-t","--table_fp",default="HIC059_mapq150_chr21.test.txt",help="TEMP: The Rao table from which to pull connections...")
 	parser.add_argument("-d","--distance",type=int,default=500,help="The allowed distance from the locus of interest for a fragment to be considered...")
 	parser.add_argument("-m","--mapq_cutoff",type=int,default=150,help="The minimum mapq score allowed...")
 	args = parser.parse_args()
@@ -74,4 +83,6 @@ if __name__ == "__main__":
 	distance = args.distance
 	mapq_cutoff = args.mapq_cutoff
 	loci = process_inputs(inputs)
-	print inputs
+	print loci
+	interactions = find_interactions(loci,table_fp,distance,mapq_cutoff)
+	print interactions
