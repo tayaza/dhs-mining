f4a8adac6c504058e399b36e7e95400bf7f3635c
Cameron Ekblad <cekb635@aucklanduni.ac.nz>
2016-01-12 18:01:37 +1300

    Added options for input libraries.

--- hiC_query.py
+++ hiC_query.py
@@ -3,15 +3,14 @@ import argparse
 import os
 import sqlite3
 from sets import Set
-import time
 
-def build_snp_index():
+def build_snp_index(snp_database_fp,snp_dir):
 	print "Building SNP index..."
-	snp_index_db = sqlite3.connect("snpIndex.db")
+	snp_index_db = sqlite3.connect(snp_database_fp)
 	snp_index = snp_index_db.cursor()
 	snp_index.execute("CREATE TABLE snps (rsID text, chr text, locus integer)")
 	snp_index.execute("CREATE INDEX id ON snps (rsID,chr,locus)")
-	for bed_fp in os.listdir("snps"):
+	for bed_fp in os.listdir(snp_dir):
 		print "\tProcessing " + bed_fp
 		bed = open("snps/" + bed_fp,'r')
 		for line in bed:
@@ -24,12 +23,12 @@ def build_snp_index():
 	snp_index_db.commit()
 	print "Done building SNP index."
 
-def process_inputs(inputs):
+def process_inputs(inputs,snp_database_fp,snp_dir):
 	snps = {}
-	if not os.path.isfile("snpIndex.db"): #Need to build SNP index
-		build_snp_index()
+	if not os.path.isfile(snp_database_fp): #Need to build SNP index
+		build_snp_index(snp_database_fp,snp_dir)
 	print "Loading SNP index..."
-	snp_index_db = sqlite3.connect("snpIndex.db")
+	snp_index_db = sqlite3.connect(snp_database_fp)
 	snp_index_db.text_factory = str
 	snp_index = snp_index_db.cursor()
 	print "Processing input..."
@@ -48,7 +47,7 @@ def process_inputs(inputs):
 			snps[snp[0]]=(snp[1],snp[2])
 	return snps
 
-def find_interactions(snps,db_connection_limit,distance,include,exclude):
+def find_interactions(snps,hic_data_dir,db_connection_limit,distance,include,exclude):
 	print "Finding interactions..."
 	if include:
 		include_set = Set(include)
@@ -57,13 +56,13 @@ def find_interactions(snps,db_connection_limit,distance,include,exclude):
 	interactions = {}
 	for snp in snps.keys():
 		interactions[snp] = []
-	for cell_line in os.listdir("hic_data"):
+	for cell_line in os.listdir(hic_data_dir):
 		if (include and not cell_line in include) or (exclude and cell_line in exclude):
 			continue
-		if os.path.isdir("hic_data/" + cell_line):
+		if os.path.isdir(hic_data_dir + '/' + cell_line):
 			print "\tSearching cell line " + cell_line
-			for replicate in os.listdir("hic_data/" + cell_line):
-				db_dir = "hic_data/" + cell_line + '/' + replicate
+			for replicate in os.listdir(hic_data_dir + '/' + cell_line):
+				db_dir = hic_data_dir + '/' + cell_line + '/' + replicate
 				if os.path.isdir(db_dir):
 					if all_chrs_present(db_dir):
 						print "\t\tSearching replicate " + replicate
@@ -91,14 +90,14 @@ def find_interactions(snps,db_connection_limit,distance,include,exclude):
 						print "\t\tWarning: database for replicate %s not in expected format." % replicate
 	return interactions
 
-def find_eqtls(interactions):
+def find_eqtls(interactions,eqtl_data_dir):
 	print "Identifying eQTLs of interest..."
 	eqtls = {} #A mapping of SNPs to different tissue types, which are in turn mapped to any eQTLs that coincide with a spatial interaction with said SNPs
 	for snp in interactions.keys():
 		eqtls[snp] = {}
-	for db in os.listdir("eQTLs"): #Iterate through databases of eQTLs by tissue type
+	for db in os.listdir(eqtl_data_dir): #Iterate through databases of eQTLs by tissue type
 		print "\tQuerying " + db[:db.rfind('.')]
-		eqtl_index_db = sqlite3.connect("eQTLs/" + db)
+		eqtl_index_db = sqlite3.connect(eqtl_data_dir + '/' + db)
 		eqtl_index_db.text_factory = str
 		eqtl_index = eqtl_index_db.cursor()
 		tissue = db[:db.rfind('.')]
@@ -124,11 +123,17 @@ if __name__ == "__main__":
 	parser.add_argument("-n","--include_cell_lines",nargs='+',help="Space-separated list of cell lines to include (others will be ignored). NOTE: Mutually exclusive with EXCLUDE_CELL_LINES.")
 	parser.add_argument("-x","--exclude_cell_lines",nargs='+',help="Space-separated list of cell lines to exclude (others will be included). NOTE: Mutually exclusive with INCLUDE_CELL_LINES.")
 	parser.add_argument("-d","--distance",type=int,default=500,help="The allowed distance from the locus of interest for a fragment to be considered.")
-	parser.add_argument("-c","--db_connection_limit",type=int,default=4)
+	parser.add_argument("-l","--db_connection_limit",type=int,default=4,help="The maximum number of database connections open simultaneously (default: 4, min: 1, max: 9)")
+	parser.add_argument("-s","--snp_database_fp",default="snpIndex.db",help="The database of SNPs to search for details on input dbSNP IDs (Optional - will try to build from files in SNP_DIR if non-existent.")
+	parser.add_argument("-t","--snp_dir",default="snps",help="The directory containing (BED) files of SNPs to use as a reference (the dbSNP Build 144 (GRCh37.p13) BED files by default).")
+	parser.add_argument("-c","--hic_data_dir",default="hic_data",help="The directory containing the directories representing cell lines of HiC experiment tables.")
+	parser.add_argument("-e","--eqtl_data_dir",default="eQTLs",help="The directory containing databases of eQTL data from various tissues (the GTEx Analysis V6 by default).")
 	args = parser.parse_args()
-	snps = process_inputs(args.inputs)
-	interactions = find_interactions(snps,args.db_connection_limit,args.distance,args.include_cell_lines,args.exclude_cell_lines)
-	eqtls = find_eqtls(interactions)
+	if args.db_connection_limit < 1 or args.db_connection_limit > 9:
+		raise InputError("DB_CONNECTION_LIMIT outside allowable range (1-9 inclusive).")
+	snps = process_inputs(args.inputs,args.snp_database_fp,args.snp_dir)
+	interactions = find_interactions(snps,args.hic_data_dir,args.db_connection_limit,args.distance,args.include_cell_lines,args.exclude_cell_lines)
+	eqtls = find_eqtls(interactions,args.eqtl_data_dir)
 	for snp in interactions.keys():
 		print snp + ':'
 		for tissue in eqtls[snp].keys():
