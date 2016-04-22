41575d1eeb776c7468e11fe8ef953b9672720bd6
Cameron Ekblad <cekb635@aucklanduni.ac.nz>
2016-04-22 18:00:33 +1200

    20160422 commit

--- hiC_query.py
+++ hiC_query.py
@@ -66,13 +66,18 @@ def process_inputs(inputs,snp_database_fp,snp_dir):
 				snps[snp[0]]=(snp[1],snp[2])
 	return snps
 
-def find_interactions(snps,hic_data_dir,distance,include,exclude,cis_interactions_only):
+def find_interactions(snps,fragment_database_fp,hic_data_dir,distance,include,exclude,cis_interactions_only):
 	print "Finding interactions..."
+	#Query fragmentIndex to find to which fragment the SNP belongs
+	fragment_index_db = sqlite3.connect(fragment_database_fp)
+	fragment_index_db.text_factory = str
+	fragment_index = fragment_index_db.cursor()
+	#Look for all interactions involving said fragment in the HiC databases
 	if include:
 		include_set = Set(include)
 	if exclude:
 		exclude_set = Set(exclude)
-	interactions = {}
+	interactions = {} #A mapping of each SNP to the fragments with which the fragment it is on interacts
 	for snp in snps.keys():
 		interactions[snp] = Set([])
 	for cell_line in os.listdir(hic_data_dir):
@@ -89,29 +94,36 @@ def find_interactions(snps,hic_data_dir,distance,include,exclude,cis_interaction
 					table_index = table_index_db.cursor()
 					for snp in snps.keys():
 						print "\t\t\tFinding interactions for " + str(snp)
+						fragment_index.execute("SELECT fragment FROM fragments WHERE chr=? AND start<=? AND end>=?",[snps[snp][0],snps[snp][1],snps[snp][1]])
+						snp_fragment_result = fragment_index.fetchone()
+						if snp_fragment == None:
+							print "Warning: error retrieving SNP fragment for SNP " + snp
+							continue
+						snp_fragment = snp_fragment_result[0]
 						snp_chr = snps[snp][0]
-						snp_locus = snps[snp][1]
 						if(cis_interactions_only):
-							for interaction in table_index.execute("SELECT chr1, locus1 FROM chr%s_interactions WHERE (chr1=? and chr2=?) and (locus2 >= ? and locus2 <= ?)" % snp_chr,[snp_chr,snp_chr,snp_locus-distance,snp_locus+distance]):
+							for interaction in table_index.execute("SELECT chr1, fragment1 FROM chr%s_interactions WHERE (chr1=? and chr2=?) and fragment2=?" % snp_chr,[snp_chr,snp_chr,snp_fragment]):
 								interactions[snp].add(interaction)
-							for interaction in table_index.execute("SELECT chr2, locus2 FROM chr%s_interactions WHERE (chr1=? and chr2=?) and (locus1 >= ? and locus1 <= ?)" % snp_chr,[snp_chr,snp_chr,snp_locus-distance,snp_locus+distance]):
+							for interaction in table_index.execute("SELECT chr2, fragment2 FROM chr%s_interactions WHERE (chr1=? and chr2=?) and fragment1=?" % snp_chr,[snp_chr,snp_chr,snp_fragment]):
 								interactions[snp].add(interaction)
 						else:
-							for interaction in table_index.execute("SELECT chr1, locus1 FROM chr%s_interactions WHERE chr2=? and (locus2 >= ? and locus2 <= ?)" % snp_chr,[snp_chr,snp_locus-distance,snp_locus+distance]):
+							for interaction in table_index.execute("SELECT chr1, fragment1 FROM chr%s_interactions WHERE chr2=? and fragment2=?)" % snp_chr,[snp_chr,snp_fragment]):
 								interactions[snp].add(interaction)
-							for interaction in table_index.execute("SELECT chr2, locus2 FROM chr%s_interactions WHERE chr1=? and (locus1 >= ? and locus1 <= ?)" % snp_chr,[snp_chr,snp_locus-distance,snp_locus+distance]):
+							for interaction in table_index.execute("SELECT chr2, fragment2 FROM chr%s_interactions WHERE chr1=? and fragment1=?)" % snp_chr,[snp_chr,snp_fragment]):
 								interactions[snp].add(interaction)
 	return interactions
 
 def find_genes(interactions,fragment_bed_fp,gene_bed_fp):
-	#For each SNP
+	for snp in interactions.keys():
 		#Generate BED file of all fragments interacting with SNP-containing fragment
+		for interaction in interactions[snp]:
+			
 		#Get intersection of this BED file with BED file detailing gene locations
 		#Return a list of genes with which SNP is interacting
 
 def find_eqtls(interactions,eqtl_data_dir):
 	print "Identifying eQTLs of interest..."
-	eqtls = {} #A mapping of SNPs to different tissue types, which are in turn mapped to any eQTLs that coincide with a spatial interaction with said SNPs
+	eqtls = {} #A mapping of SNPs to genes with which they have an eQTL relationship, which in turn maps to a list of tissues in which this eQTL occurs
 	for snp in interactions.keys():
 		eqtls[snp] = {} #Stores eQTLs relevant to this SNP, and, for each eQTL, a list of the tissues in which it is found
 	for db in os.listdir(eqtl_data_dir): #Iterate through databases of eQTLs by tissue type
@@ -169,13 +181,14 @@ if __name__ == "__main__":
 	parser.add_argument("-t","--snp_dir",default="snps",help="The directory containing (BED) files of SNPs to use as a reference (the dbSNP Build 144 (GRCh37.p13) BED files by default).")
 	parser.add_argument("-c","--hic_data_dir",default="hic_data",help="The directory containing the directories representing cell lines of HiC experiment tables.")
 	parser.add_argument("-f","--fragment_bed_fp",default="Homo_sapiens.ensembl.release-74.MboI.fragments.bed",help="Bed file detailing the start and end points of HiC experiment fragments (hg19 fragmented by MboI by default.)")
+	parser.add_argument("-b","--fragment_database_fp",default="fragmentIndex.db",help="The database of fragments to search when assigning SNPs to fragments (GRCh37 digested by MboI by default.)")
 	parser.add_argument("-g","--gene_bed_fp",default="hg19_genes.bed",help="Bed file detailing locations of genes in genome (the UCSC known gene list for hg19 by default).")
 	parser.add_argument("-e","--eqtl_data_dir",default="eQTLs",help="The directory containing databases of eQTL data from various tissues (the GTEx Analysis V6 by default).")
 	parser.add_argument("-o","--output_dir",default="hiCquery_output",help="The directory in which to output results (\"hiCquery_output\" by default).")
 	parser.add_argument("-j","--cis_interactions_only",action="store_true",default=False,help="Consider only cis interactions, i.e. interactions occurring on the same chromosome.")
 	args = parser.parse_args()
 	snps = process_inputs(args.inputs,args.snp_database_fp,args.snp_dir)
-	interactions = find_interactions(snps,args.hic_data_dir,args.distance,args.include_cell_lines,args.exclude_cell_lines)
+	interactions = find_interactions(snps,args.fragment_database_fp,args.hic_data_dir,args.distance,args.include_cell_lines,args.exclude_cell_lines)
 	#genes = find_genes(interactions,args.fragment_bed_fp,args.gene_bed_fp)
 	eqtls = find_eqtls(interactions,args.eqtl_data_dir)
 	produce_output(snps,interactions,eqtls,args.output_dir)
